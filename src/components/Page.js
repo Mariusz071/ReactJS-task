913d39b7cade59f345cf01889c8784b11126961f
Mariusz Siewruk <siewruk.mariusz92@gmail.com>
2018-12-17 10:16:06 +0000

    container component

---
+++ src/components/Page.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import UserToolbar from './UserToolbar';
+
+
+export default class Page extends Component {
+  render() {
+    return (
+         <div>
+            <UserToolbar/>
+        </div>
+    );
+  }
+}
+
