836e487886444409c6503a366333845afb3bffac
Mariusz Siewruk <siewruk.mariusz92@gmail.com>
2018-12-17 10:18:06 +0000

    example component to show react router working

---
+++ src/components/RouterLinkExample.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const RouterExample = (props) => {
+    return (
+        <div>
+            <h1>Just an example to show that I know how to use react router. :)</h1>
+        </div>)
+}
+
+export default RouterExample;
