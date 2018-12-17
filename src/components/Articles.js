0ff89605fa3fcfba6ad331348719b5b3419f58b3
Mariusz Siewruk <siewruk.mariusz92@gmail.com>
2018-12-17 15:05:06 +0000

    Added articles list

---
+++ src/components/Articles.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+const Articles = () => {
+    return(
+        <div className='Articles'>
+            <div className='Articles__row'>
+                <article>
+                    <p>Otam rem aperiam</p>
+                    <p>Iqsa quae</p>
+                    <p>Unde omnis</p>
+                    <p>Volptatem (dubrnem)</p>
+                </article>
+
+                <article>
+                    <p>Perspiciatis Unde</p>
+                    <p>Architecto beatae vitae</p>
+                    <p>Perspiciatis Unde</p>
+                    <p>Architecto beatae vitae</p>
+                </article>
+
+                <article>
+                    <p>architecto beatae vitae</p>
+                </article>
+            </div>
+
+            <div className='Articles__row'>
+                <article>
+                    <p>Totam</p>
+                    <p>Iste Natus</p>
+                    <p>Omnis Iste Natur Erro</p>
+                    <p>Explicabo</p>
+                </article>
+
+                <article>
+                    <p>5.0</p>
+                    <p>Omnis iste natus</p>
+                    <p>Accusantium dolormque laudant</p>
+                    <p>Accusantium dolor</p>
+                </article>
+            </div>
+
+        </div>
+    )
+}
+
+export default Articles;
