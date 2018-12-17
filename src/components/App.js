4ea09504f6e86ae75f24ae838fb483e8e048ba32
Mariusz Siewruk <siewruk.mariusz92@gmail.com>
2018-12-17 10:15:23 +0000

    added main App component

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Route, Switch } from "react-router-dom";
+import CssBaseline from '@material-ui/core/CssBaseline';
+import MainBar from './MainBar';
+import Page from './Page';
+import '../assets/styles/index.scss'
+import RouterExample from './RouterLinkExample';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <CssBaseline />
+        <MainBar />
+        <Switch>
+          <Route exact path="/routerlinkexample" component={RouterExample} />
+          <Route exact path ='/' component={Page}/>
+        </Switch>
+      </div>
+    )
+  }
+}
+
